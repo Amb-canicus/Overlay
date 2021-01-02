9509064949d27e0d0c0e8339b42df24028dad809
Matias <amb19821982@gmail.com>
2021-01-02 13:00:42 -0300

    overlay terminado

---
+++ main.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById('nav').style.left="0px";
+    document.getElementById('openNav').style.display="none";
+}
+
+function closeNav() {
+    document.getElementById('nav').style.left="-250px";
+    document.getElementById('openNav').style.display='block';
+    
+}
+
